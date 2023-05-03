c832e54fef7fa6bf98cf4e89a7d11ff7aaa543a0
Efe Berk Çetinbaş <berkefefast@gmail.com>
2023-05-03 16:30:03 +0300

    - Environment and Characters Added - Boss Added - Fruit Ball Mesh Mechanic Added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,5 +6,24 @@ public class Player : MonoBehaviour
 {
     public int id;
 
+    private int randomIndex;
     public ParticleSystem particle;
+
+    public List<Sprite> sprites=new List<Sprite>();
+
+    private SpriteRenderer spriteRenderer;
+    private void Start() 
+    {
+        spriteRenderer=GetComponent<SpriteRenderer>();
+        MakeRandomSprite(spriteRenderer);
+        
+    }
+
+    private Sprite MakeRandomSprite(SpriteRenderer spriteRenderer)
+    {
+        randomIndex=Random.Range(0,sprites.Count);
+        spriteRenderer.sprite=sprites[randomIndex];
+        return spriteRenderer.sprite;
+        
+    }
 }
